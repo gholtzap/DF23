5445bd4a8263cecbb30a62463d5f29ca1a86d74c
gholtzap <gholtzap@asu.edu>
2023-03-25 11:47:42 -0700

    heatmap adjustments + organization of files

--- heatmap.py
+++ heatmap.py
@@ -11,14 +11,18 @@ income_data = pd.read_csv('grouped_data.csv')
 state_avg_income = income_data.groupby(c1)[c2].mean().reset_index()
 
 # Read the shapefile for US states
-us_states = gpd.read_file('tl_2020_us_state.shp')
+us_states = gpd.read_file('maps/tl_2020_us_state.shp')
 
 # Merge the shapefile with the average annual income data
 us_states_data = us_states.merge(state_avg_income, left_on='NAME', right_on=c1)
 
 # Plot the heatmap
-fig, ax = plt.subplots(1, figsize=(15, 15))
-us_states_data.plot(column=c2, cmap='coolwarm', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
-ax.set_title('Average Annual Income by State')
+fig, ax = plt.subplots(1, figsize=(15, 8))
+us_states_data.plot(column=c2, cmap='coolwarm', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True, legend_kwds={'label': "Average Annual Income", 'orientation': "horizontal", 'shrink': 0.5, 'pad': 0.02})
+ax.set_title('Average Annual Income by State', fontdict={'fontsize': 14}, pad=20)
 ax.set_axis_off()
 plt.show()
+
+
+# save as PNG
+#plt.savefig("us_income_heatmap.png", dpi=300)
