99bba16b839df18b151e8f9cac86c51cc278ce9d
gholtzap <gholtzap@asu.edu>
2023-03-25 13:57:32 -0700

    filter unwanted responses

--- state_filter_sort.py
+++ state_filter_sort.py
@@ -1,15 +1,17 @@
 import pandas as pd
-import zipcodes
 import numpy as np
 
 c1 = 'StateName'
 c2 = 'AnnualIncome'
 
 # Read the CSV file
-data = pd.read_csv('data/clients_first2000.csv')
+data = pd.read_csv('data/clients.csv')
+
+# Filter out any 0 incomes | Reduces the dataset to about 330,000 to about 250,000
+filtered_data = data.query("AnnualIncome != 0 & AnnualIncome <= 1000000").dropna(subset=[c2])
 
 # Group by 'StateName' and compute the mean of 'AnnualIncome' for each group
-grouped_data = data.groupby(c1)[c2].mean().reset_index()
+grouped_data = filtered_data.groupby(c1)[c2].mean().reset_index()
 
 # Save the aggregated data to a new CSV file
 grouped_data.to_csv('data/grouped_data.csv', index=False)
