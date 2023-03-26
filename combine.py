cf62bf17eb4efda3c34dcd7176884190b012c9ee
gholtzap <gholtzap@asu.edu>
2023-03-25 19:24:28 -0700

    added comments

--- combine.py
+++ combine.py
@@ -3,32 +3,35 @@ import pandas as pd
 # Read the CSV file
 data = pd.read_csv('data/housing_data.csv')
 
-# init array
+# declare dicts
 scores_housing = {
 }
 
 scores_income = {
 }
 
-# Specify the column name you want to sum
+# Storing column names as variables
 column_count = 'Count'
 column_state = 'State'
 column_income_state = 'StateName'
 column_income = 'AnnualIncome'
 
-# Iterate through the rows of the DataFrame and add the value to the total variable
+# Iterate through the rows of the data and add a key:value to the dict in the form of State:Value
 for index, row in data.iterrows():
     scores_housing[row[column_state]] = row[column_count]
-    
+
+# Once we are done with housing_data, open next file  
 data = pd.read_csv('data/grouped_data.csv')
 
+# Same iteration as above
 for index, row in data.iterrows():
     scores_income[row[column_income_state]] = row[column_income]
 
+# ↓ Combining the two dicts created above ↓
 combined_dict = scores_housing.copy()  # Start with a copy of dict1
 
 for key, value in scores_income.items():
     if key in combined_dict:
-        combined_dict[key] *= (1/value)  # If the key exists, add the value to the existing key
+        combined_dict[key] *= (1/value)  # If the key exists, multiply by the inverse of the income
 
 print(combined_dict)
